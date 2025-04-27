7ef5c1d53d5c8bd666eaef7d316644114bafadfd
Petros Barreto da silva <>
2025-04-26 22:16:59 -0300

    fix: converte nome do repositório para lowercase

--- init_script_canary.sh
+++ init_script_canary.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+REPO_LOWER=$(echo ${GITHUB_REPOSITORY} | tr '[:upper:]' '[:lower:]')
 echo "${TF_VAR_api_key}" | docker login ${REGISTRY} --username "${TF_VAR_api_key}" --password-stdin
-docker pull ${REGISTRY}/${GITHUB_REPOSITORY}:canary
+docker pull ${REGISTRY}/${MAGALU_REGION}/${REPO_LOWER}:canary
 docker stop react-app-canary || true
 docker rm react-app-canary || true
-docker run -d --name react-app-canary --restart unless-stopped -p 3001:80 ${REGISTRY}/${GITHUB_REPOSITORY}:canary
+docker run -d --name react-app-canary --restart unless-stopped -p 3001:80 ${REGISTRY}/${MAGALU_REGION}/${REPO_LOWER}:canary
