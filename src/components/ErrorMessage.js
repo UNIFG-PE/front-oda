d3fe8a162b9cdecdbf02d9cc36c65d4d2c72e1a3
Erick Patrick <erickckpatrick17@gmail.com>
2025-05-08 13:43:27 -0300

    chore: remove pages from components folder

--- src/components/ErrorMessage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ErrorMessage = ({ message }) => {
-  return <div className="erro">{message}</div>;
-};
-
-export default ErrorMessage;
--- src/components/Logo.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import logo from "../assets/logo.png";
-
-const Logo = () => {
-  return <img src={logo} alt="Logo" className="logoImage" />;
-};
-
-export default Logo;
